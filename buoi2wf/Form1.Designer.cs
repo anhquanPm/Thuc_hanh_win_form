8eccb6bf86090a826a32c9e12879ae2b92103757
thocodeanhquan <thocodeanhquan@gmail.com>
2024-02-26 11:20:58 +0700

    Add project files.

---
+++ buoi2wf/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace buoi2wf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tb_ma_sv = new System.Windows.Forms.TextBox();
+            this.tb_ten_sv = new System.Windows.Forms.TextBox();
+            this.tb_sdt = new System.Windows.Forms.TextBox();
+            this.tb_dia_chi = new System.Windows.Forms.TextBox();
+            this.btn_them = new System.Windows.Forms.Button();
+            this.btn_bo_qua = new System.Windows.Forms.Button();
+            this.btn_edit = new System.Windows.Forms.Button();
+            this.btn_xoa = new System.Windows.Forms.Button();
+            this.btn_tim_kiem = new System.Windows.Forms.Button();
+            this.rad_nam = new System.Windows.Forms.RadioButton();
+            this.rad_nu = new System.Windows.Forms.RadioButton();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.rad_nu);
+            this.groupBox1.Controls.Add(this.rad_nam);
+            this.groupBox1.Controls.Add(this.btn_tim_kiem);
+            this.groupBox1.Controls.Add(this.btn_xoa);
+            this.groupBox1.Controls.Add(this.btn_edit);
+            this.groupBox1.Controls.Add(this.btn_bo_qua);
+            this.groupBox1.Controls.Add(this.btn_them);
+            this.groupBox1.Controls.Add(this.tb_dia_chi);
+            this.groupBox1.Controls.Add(this.tb_sdt);
+            this.groupBox1.Controls.Add(this.tb_ten_sv);
+            this.groupBox1.Controls.Add(this.tb_ma_sv);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(39, 22);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(751, 353);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin sinh viên";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 20);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Mã sinh viên";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Họ tên sinh viên";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Số điện thoại";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Địa chỉ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 254);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Ngày sinh";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 302);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Giới tính";
+            // 
+            // tb_ma_sv
+            // 
+            this.tb_ma_sv.Location = new System.Drawing.Point(246, 62);
+            this.tb_ma_sv.Name = "tb_ma_sv";
+            this.tb_ma_sv.Size = new System.Drawing.Size(291, 27);
+            this.tb_ma_sv.TabIndex = 6;
+            this.tb_ma_sv.TextChanged += new System.EventHandler(this.tb_ma_sv_TextChanged);
+            // 
+            // tb_ten_sv
+            // 
+            this.tb_ten_sv.Location = new System.Drawing.Point(246, 110);
+            this.tb_ten_sv.Name = "tb_ten_sv";
+            this.tb_ten_sv.Size = new System.Drawing.Size(291, 27);
+            this.tb_ten_sv.TabIndex = 7;
+            this.tb_ten_sv.TextChanged += new System.EventHandler(this.tb_ten_sv_TextChanged);
+            // 
+            // tb_sdt
+            // 
+            this.tb_sdt.Location = new System.Drawing.Point(246, 158);
+            this.tb_sdt.Name = "tb_sdt";
+            this.tb_sdt.Size = new System.Drawing.Size(291, 27);
+            this.tb_sdt.TabIndex = 8;
+            this.tb_sdt.TextChanged += new System.EventHandler(this.tb_sdt_TextChanged);
+            // 
+            // tb_dia_chi
+            // 
+            this.tb_dia_chi.Location = new System.Drawing.Point(246, 206);
+            this.tb_dia_chi.Name = "tb_dia_chi";
+            this.tb_dia_chi.Size = new System.Drawing.Size(291, 27);
+            this.tb_dia_chi.TabIndex = 9;
+            this.tb_dia_chi.TextChanged += new System.EventHandler(this.tb_dia_chi_TextChanged);
+            // 
+            // btn_them
+            // 
+            this.btn_them.Image = ((System.Drawing.Image)(resources.GetObject("btn_them.Image")));
+            this.btn_them.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_them.Location = new System.Drawing.Point(595, 60);
+            this.btn_them.Name = "btn_them";
+            this.btn_them.Size = new System.Drawing.Size(117, 30);
+            this.btn_them.TabIndex = 1;
+            this.btn_them.Text = "Thêm mới";
+            this.btn_them.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_them.UseVisualStyleBackColor = true;
+            this.btn_them.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_bo_qua
+            // 
+            this.btn_bo_qua.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_bo_qua.Location = new System.Drawing.Point(595, 110);
+            this.btn_bo_qua.Name = "btn_bo_qua";
+            this.btn_bo_qua.Size = new System.Drawing.Size(117, 30);
+            this.btn_bo_qua.TabIndex = 13;
+            this.btn_bo_qua.Text = "Bỏ qua";
+            this.btn_bo_qua.UseVisualStyleBackColor = true;
+            // 
+            // btn_edit
+            // 
+            this.btn_edit.Image = ((System.Drawing.Image)(resources.GetObject("btn_edit.Image")));
+            this.btn_edit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_edit.Location = new System.Drawing.Point(595, 158);
+            this.btn_edit.Name = "btn_edit";
+            this.btn_edit.Size = new System.Drawing.Size(117, 30);
+            this.btn_edit.TabIndex = 14;
+            this.btn_edit.Text = "Chỉnh sửa";
+            this.btn_edit.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_edit.UseVisualStyleBackColor = true;
+            // 
+            // btn_xoa
+            // 
+            this.btn_xoa.Image = ((System.Drawing.Image)(resources.GetObject("btn_xoa.Image")));
+            this.btn_xoa.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_xoa.Location = new System.Drawing.Point(595, 206);
+            this.btn_xoa.Name = "btn_xoa";
+            this.btn_xoa.Size = new System.Drawing.Size(117, 30);
+            this.btn_xoa.TabIndex = 15;
+            this.btn_xoa.Text = "Xóa bỏ";
+            this.btn_xoa.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_xoa.UseVisualStyleBackColor = true;
+            // 
+            // btn_tim_kiem
+            // 
+            this.btn_tim_kiem.Image = ((System.Drawing.Image)(resources.GetObject("btn_tim_kiem.Image")));
+            this.btn_tim_kiem.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_tim_kiem.Location = new System.Drawing.Point(595, 254);
+            this.btn_tim_kiem.Name = "btn_tim_kiem";
+            this.btn_tim_kiem.Size = new System.Drawing.Size(117, 30);
+            this.btn_tim_kiem.TabIndex = 16;
+            this.btn_tim_kiem.Text = "Tìm kiếm";
+            this.btn_tim_kiem.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_tim_kiem.UseVisualStyleBackColor = true;
+            // 
+            // rad_nam
+            // 
+            this.rad_nam.AutoSize = true;
+            this.rad_nam.Location = new System.Drawing.Point(246, 300);
+            this.rad_nam.Name = "rad_nam";
+            this.rad_nam.Size = new System.Drawing.Size(57, 24);
+            this.rad_nam.TabIndex = 17;
+            this.rad_nam.TabStop = true;
+            this.rad_nam.Text = "Nam";
+            this.rad_nam.UseVisualStyleBackColor = true;
+            this.rad_nam.CheckedChanged += new System.EventHandler(this.rad_nam_CheckedChanged);
+            // 
+            // rad_nu
+            // 
+            this.rad_nu.AutoSize = true;
+            this.rad_nu.Location = new System.Drawing.Point(334, 300);
+            this.rad_nu.Name = "rad_nu";
+            this.rad_nu.Size = new System.Drawing.Size(48, 24);
+            this.rad_nu.TabIndex = 18;
+            this.rad_nu.TabStop = true;
+            this.rad_nu.Text = "Nữ";
+            this.rad_nu.UseVisualStyleBackColor = true;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "dd/MM/yyyy";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(246, 261);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(291, 27);
+            this.dateTimePicker1.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(832, 415);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Thông tin sinh viên";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tb_dia_chi;
+        private System.Windows.Forms.TextBox tb_sdt;
+        private System.Windows.Forms.TextBox tb_ten_sv;
+        private System.Windows.Forms.TextBox tb_ma_sv;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_tim_kiem;
+        private System.Windows.Forms.Button btn_xoa;
+        private System.Windows.Forms.Button btn_edit;
+        private System.Windows.Forms.Button btn_bo_qua;
+        private System.Windows.Forms.Button btn_them;
+        private System.Windows.Forms.RadioButton rad_nu;
+        private System.Windows.Forms.RadioButton rad_nam;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ buoi2wf/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Drawing.Design;
+using System.Windows.Forms;
+
+namespace buoi2wf
+{
+    public partial class Form1 : Form
+    {
+        bool check = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //errorProvider1.Clear();
+            btn_them.Enabled = false;
+            //if (tb_ma_sv.Text == string.Empty)
+            //{
+            //    errorProvider1.SetError(tb_ma_sv, "Bạn chưa nhập mã sinh viên");
+            //}
+
+            //if (tb_sdt.Text == string.Empty)
+            //{
+            //    errorProvider1.SetError(tb_sdt, "Bạn chưa nhập số điện thoại");
+            //}
+
+            //errorProvider1.Clear();
+            //if (tb_ten_sv.Text == string.Empty)
+            //{
+            //    errorProvider1.SetError(tb_ten_sv, "Bạn chưa nhập tên sinh viên");
+            //}
+            rad_nam.Checked = true;
+
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //if(check == true)
+            //{
+            //    DialogResult result = MessageBox.Show("Vui lòng nhập đủ thông tin");
+
+            //}
+ 
+        }
+
+        private void tb_ma_sv_TextChanged(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+            if (tb_ma_sv.Text != string.Empty)
+            {
+                btn_them.Enabled = true;
+               
+            }
+            else
+            {
+                btn_them.Enabled = false;
+            }
+
+            if (tb_ma_sv.Text == string.Empty)
+            {
+                errorProvider1.SetError(tb_ma_sv,"Bạn chưa nhập mã sinh viên");
+               
+            }
+        }
+
+        private void tb_sdt_TextChanged(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+            if(!IsNumber(tb_sdt.Text))
+            {
+                btn_them.Enabled = false;
+                errorProvider1.SetError(tb_sdt, "Sai định dạng sđt");
+                
+            }
+        }
+
+        private bool IsNumber(string val)
+        {
+            foreach(char c in val)
+            {
+                if(!char.IsDigit(c)) return false;
+            }
+            return true;
+        }
+
+        private void tb_ten_sv_TextChanged(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+            if(tb_ten_sv.Text == string.Empty)
+            {
+                errorProvider1.SetError(tb_ten_sv, "Bạn chưa nhập tên sinh viên");
+               
+            }
+        }
+
+        private void tb_dia_chi_TextChanged(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+            if (tb_dia_chi.Text == string.Empty)
+            {
+                errorProvider1.SetError(tb_dia_chi, "Bạn chưa nhập địa chỉ");
+              
+            }
+        }
+
+        private void rad_nam_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
