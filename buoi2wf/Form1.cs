bd1ef6c49cd93c5e654e3503f37068fa05a0f72d
thocodeanhquan <thocodeanhquan@gmail.com>
2024-02-26 23:04:05 +0700

    fix error provirder

--- buoi2wf/Form1.cs
+++ buoi2wf/Form1.cs
@@ -1,27 +1,12 @@
 ﻿using System;
-using System.Drawing.Design;
-using System.Threading;
 using System.Windows.Forms;
 
 namespace buoi2wf
 {
     public partial class Form1 : Form
     {
-        bool isRunning = true;
-        public void CheckBtnAdd()
-        {
-            
-            if(tb_ma_sv.Text == string.Empty || tb_ten_sv.Text == string.Empty || tb_sdt.Text == string.Empty
-                || tb_dia_chi.Text == string.Empty)
-            {
-                btn_them.Enabled = false;
-            }
-            else
-            {
-                btn_them.Enabled = true;
-            }
-        }
-        bool check = false;
+
+        
         public Form1()
         {
             InitializeComponent();
@@ -45,42 +30,77 @@ namespace buoi2wf
 
         private void button1_Click(object sender, EventArgs e)
         {
-           
-                MessageBox.Show("Thêm thành công");
 
+            bool check = true;
+            errorProvider1.Clear();
+            if (tb_ma_sv.Text == string.Empty)
+            {
+                errorProvider1.SetError(tb_ma_sv, "Bạn chưa nhập mã sinh viên");
+                check = false;
+            }
+
+            //if (!IsNumber(tb_sdt.Text))
+            //{
+            //    errorProvider1.SetError(tb_sdt, "Sai định dạng sđt");
+            //    check = true;
+
+            //}
+
+
+
+            int sdt;
+            if (int.TryParse(tb_sdt.Text, out sdt) == false)
+            {
+                errorProvider1.SetError(tb_sdt, "Sai định dạng tuổi");
+                check = false;
+            }
+
+
+            if (tb_sdt.Text == string.Empty)
+            {
+                errorProvider1.SetError(tb_sdt, "Bạn chưa nhập số điện thoại");
+                check = false;
+            }
+
+
+
+
+            if (tb_ten_sv.Text == string.Empty)
+            {
+                errorProvider1.SetError(tb_ten_sv, "Bạn chưa nhập tên sinh viên");
+                check = false;
+            }
+
+
+
+            if (tb_dia_chi.Text == string.Empty)
+            {
+                errorProvider1.SetError(tb_dia_chi, "Bạn chưa nhập địa chỉ");
+                check = false;
+            }
+
+
+            if(rad_nu.Checked == false && rad_nam.Checked == false)
+            {
+                errorProvider1.SetError(rad_nu, "Bạn chưa chọn giới tính");
+                errorProvider1.SetError(rad_nam, "Bạn chưa chọn giới tính");
+                check = false;
+            }
+
+            if (check)
+            {
+                MessageBox.Show("Thêm thành công");
+            }
         }
 
         private void tb_ma_sv_TextChanged(object sender, EventArgs e)
         {
-            errorProvider1.Clear();
-            CheckBtnAdd();
-            if (tb_ma_sv.Text == string.Empty)
-            {
-                errorProvider1.SetError(tb_ma_sv,"Bạn chưa nhập mã sinh viên");
-                check = true;
-            }
-            else
-            {
-                check = false;
-            }
+
         }
 
         private void tb_sdt_TextChanged(object sender, EventArgs e)
         {
-
-            errorProvider1.Clear();
-            CheckBtnAdd();
-            if (!IsNumber(tb_sdt.Text))
-            {
-                btn_them.Enabled = false;
-                errorProvider1.SetError(tb_sdt, "Sai định dạng sđt");
-                check=true;
-
-            }
-            else
-            {
-                check = false;
-            }
+      
         }
 
         private bool IsNumber(string val)
@@ -94,32 +114,12 @@ namespace buoi2wf
 
         private void tb_ten_sv_TextChanged(object sender, EventArgs e)
         {
-            errorProvider1.Clear();
-            CheckBtnAdd();
-            if (tb_ten_sv.Text == string.Empty)
-            {
-                errorProvider1.SetError(tb_ten_sv, "Bạn chưa nhập tên sinh viên");
-                check = true;
-            }
-            else
-            {
-                check = false;
-            }
+
         }
 
         private void tb_dia_chi_TextChanged(object sender, EventArgs e)
         {
-            errorProvider1.Clear();
-            CheckBtnAdd();
-            if (tb_dia_chi.Text == string.Empty)
-            {
-                errorProvider1.SetError(tb_dia_chi, "Bạn chưa nhập địa chỉ");
-                check = true;
-            }
-            else
-            {
-                check = false;
-            }
+
         }
 
         private void rad_nam_CheckedChanged(object sender, EventArgs e)
@@ -134,15 +134,12 @@ namespace buoi2wf
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            isRunning = false;
         }
 
         
 
         private void Form1_Activated(object sender, EventArgs e)
         {
-            CheckBtnAdd();
-            Console.WriteLine("Hello");
         }
 
         
